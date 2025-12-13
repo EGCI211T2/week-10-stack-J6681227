93d41ce212a85979e1b6975f712da4a09a6980bd
J6681227 <pawis.nit@student.mahidol.ac.th>
2025-12-13 08:11:44 +0000

    sg

--- main2.cpp
+++ main2.cpp
@@ -1,46 +1,44 @@
-// charstack.h
-#ifndef CHARSTACK_H
-#define CHARSTACK_H
-
+#include "charstack.h"
 #include <iostream>
 using namespace std;
 
-class CharNode {
-public:
-    char data;
-    CharNode* next;
-    CharNode(char val) : data(val), next(nullptr) {}
-    ~CharNode() {}
-};
+bool isMatching(char open, char close) {
+    if (open == '(' && close == ')') return true;
+    if (open == '{' && close == '}') return true;
+    if (open == '[' && close == ']') return true;
+    return false;
+}
 
-class CharStack {
-private:
-    CharNode* top;
-public:
-    CharStack() : top(nullptr) {}
-    ~CharStack() {
-        while (top) pop();
+int main() {
+    CharStack s;
+    string expr;
+
+    cout << "Enter expression: ";
+    cin >> expr;
+
+    for (int i = 0; i < expr.length(); i++) {
+        char c = expr[i];
+
+        if (c == '(' || c == '{' || c == '[') {
+            s.push(c);
+        }
+        else if (c == ')' || c == '}' || c == ']') {
+            if (s.isEmpty()) {
+                cout << "Not Balanced" << endl;
+                return 0;
+            }
+            char top = s.pop();
+            if (!isMatching(top, c)) {
+                cout << "Not Balanced" << endl;
+                return 0;
+            }
+        }
     }
 
-    void push(char c) {
-        CharNode* n = new CharNode(c);
-        n->next = top;
-        top = n;
-    }
+    if (s.isEmpty())
+        cout << "Balanced" << endl;
+    else
+        cout << "Not Balanced" << endl;
 
-    char pop() {
-        if (!top) return '\0';
-        char c = top->data;
-        CharNode* t = top;
-        top = top->next;
-        delete t;
-        cout << "Popped: " << c << endl; // print during pop
-        return c;
-    }
-
-    bool isEmpty() {
-        return top == nullptr;
-    }
-};
-
-#endif
+    return 0;
+}
